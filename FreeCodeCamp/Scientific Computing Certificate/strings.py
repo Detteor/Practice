20786f58eafe639ce68b921bff10f95a22a31396
benkinnaman@gmail.com <34143047+Detteor@users.noreply.github.com>
2021-04-20 22:21:16 -0400

    New Lessons

---
+++ FreeCodeCamp/Scientific Computing Certificate/strings.py
@@ -0,0 +1,9 @@
+# fruit = "banana"
+# print (fruit[1])
+
+fruit = "banana"
+index = 0
+while index < len(fruit):
+    letter = fruit[index]
+    print (index,letter)
+    index = index +1
--- FreeCodeCamp/Scientific Computing Certificate/zork.py
+++ FreeCodeCamp/Scientific Computing Certificate/zork.py
@@ -14,8 +14,35 @@
 #     print (count, sum, value)
 # print ('After', count, sum, sum / count)
 
+# print ('Before')
+# for value in [9,41,12,3,74,15]:
+#     if value > 20:
+#         print ('Large Number', value)
+# print ('After')
+
+# found = False
+# print ('Before', found)
+# for value in [9,41,12,3,74,15]:
+#     if value == 3:
+#         found = True
+#         break
+#     print (found, value)
+# print ('After', found)
+
+# largest_so_far = -1 
+# print ('Before', largest_so_far)
+# for the_num in [9,41,12,3,74,15]:
+#     if the_num > largest_so_far:
+#         largest_so_far = the_num
+#     print (largest_so_far, the_num)
+# print ('After', largest_so_far)
+
+smallest = None
 print ('Before')
 for value in [9,41,12,3,74,15]:
-    if value > 20:
-        print ('Large Number', value)
-print ('After')
+    if smallest is None:
+        smallest = value
+    elif value < smallest:
+        smallest = value
+    print (smallest, value)
+print ('After', smallest)
